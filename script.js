5c310dbecc91fdb50641ff023841b39163c556b8
Marcio Leandro da Silva Cruz <72270210+MarcioLeandro8@users.noreply.github.com>
2025-06-12 10:41:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openTab(evt, tabName) {
+  const tabContents = document.querySelectorAll('.tab-content');
+  const tabLinks = document.querySelectorAll('.tab-link');
+
+  tabContents.forEach((tab) => tab.classList.remove('active'));
+  tabLinks.forEach((btn) => btn.classList.remove('active'));
+
+  document.getElementById(tabName).classList.add('active');
+  evt.currentTarget.classList.add('active');
+}
